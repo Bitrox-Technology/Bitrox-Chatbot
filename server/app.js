140c3218bab252d7c947e5a985ba3822bb8c5559
Ashish <ashish.bitrox@gmail.com>
2024-12-04 17:49:32 +0530

    update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 app.use(helmet())
 app.use(cors({
-    "origin": "https://bitrox-chatbot-frontend-68jdzhcnd-ashishs-projects-cab18589.vercel.app/",
+    "origin": "https://bitrox-chatbot-frontend-68jdzhcnd-ashishs-projects-cab18589.vercel.app",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
