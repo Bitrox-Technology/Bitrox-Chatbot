190a219d5fa760b90afaff71fac4f01a5c908427
Ashish <ashish.bitrox@gmail.com>
2024-12-05 17:48:13 +0530

    update in server

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'https://bitrox-chatbot-frontend.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Origin', 'Content-Type', 'Authorization'],
         credentials: true,
